699c6774ff7c3c52d478a319dab25ab9887bf23d
ntalbs <ntalbsen@gmail.com>
2020-03-19 22:53:59 +0000

    Update test

--- src/test/java/ntalbs/velociraptor/VelociraptorVerticleTest.java
+++ src/test/java/ntalbs/velociraptor/VelociraptorVerticleTest.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
-import ntalbs.velociraptor.echo.EchoHandler;
+import ntalbs.velociraptor.handler.base.DefaultHandler;
+import ntalbs.velociraptor.handler.echo.EchoHandler;
+import ntalbs.velociraptor.handler.proxy.ProxyHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,8 +16,9 @@ class VelociraptorVerticleTest {
 
   @BeforeEach
   void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    ObjectMapper mapper = new ObjectMapper();
     vertx.deployVerticle(
-      new VelociraptorVerticle(new EchoHandler(new ObjectMapper())),
+      new VelociraptorVerticle(new EchoHandler(mapper), new ProxyHandler(), new DefaultHandler(mapper)),
       testContext.succeeding(id -> testContext.completeNow())
     );
   }
