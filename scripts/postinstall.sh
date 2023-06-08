11bfe4eb40bfee80f720e309983db398da3f52be
ntalbs <ntalbs@users.noreply.github.com>
2023-06-08 09:30:38 +0100

    Update postinstall.sh
    Use `tar` instead of `jar`. `jar` is not available.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 cd /var
-jar -xf reflexive-java-0.0.1.zip
+tar -xf reflexive-java-0.0.1.tar
 mv reflexive-java-0.0.1 reflexive-java
 rm reflexive-java-0.0.1.zip
 chmod u+x reflexive-java/bin/reflexive-java
